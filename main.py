b3574a10472877a3bd2a85393595af93c355a111
Alexander Nguyen <altecx.nguyen@gmail.com>
2023-11-20 20:47:46 -0500

    Clean up

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import json
 
 
 def train():
+    # Dictionary of index and respective classes
     lookup = dict()
     reverselookup = dict()
     for count, label in enumerate(os.listdir('./leapGestRecog/leapGestRecog/00/')):
@@ -21,6 +22,7 @@ def train():
             lookup[label] = count
             reverselookup[count] = label
 
+    # Save dictionary of index and class
     with open("reverselookup.json", "w") as file:
         json.dump(reverselookup, file)
 
@@ -35,16 +37,21 @@ def train():
 
     images = []
     labels = []
+    # Iterate through 10 sample folders
     for i in range(10):
         print(i)
+        # Iterate through the different gestures of each sample
         for j in os.listdir(os.path.join('./leapGestRecog/leapGestRecog', '0' + str(i))):
             if not j.startswith('.'):
+                # Iterate through the photos
                 for k in os.listdir(os.path.join('./leapGestRecog/leapGestRecog', '0'+str(i), j)):
                     img_path = os.path.join('./leapGestRecog/leapGestRecog', '0'+str(i), j, k)
+                    # Resize and normalize
                     img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
                     img = cv2.resize(img, (200, 200))
                     img_normalized = img / 255.0
 
+                    # Append to list of images and respective labels
                     images.append(img_normalized)
                     labels.append(lookup[j])
 
@@ -69,7 +76,7 @@ def train():
     labels = np.array(labels)
     print("Finished loading data")
 
-
+    # Split training and testing data
     X_train, X_test, y_train, y_test = train_test_split(
         images,
         labels,
@@ -78,6 +85,7 @@ def train():
         random_state=42,
     )
 
+    # CNN model
     model = tf.keras.models.Sequential([
         tf.keras.layers.Conv2D(filters=8, kernel_size=3, activation='relu', padding='same', input_shape=(200, 200, 1)),
         tf.keras.layers.MaxPooling2D(),
@@ -90,15 +98,20 @@ def train():
         tf.keras.layers.Dense(units=len(lookup), activation='softmax')
     ])
 
+    # Train on dataset
     model.compile(loss="sparse_categorical_crossentropy", optimizer=RMSprop(learning_rate=0.001), metrics=['accuracy'])
     history = model.fit(X_train, y_train, epochs=5, validation_data=(X_test, y_test))
 
+    # Save model
     model.save("trainedmodel.h5")
 
 
 def predict():
+    # Load the dictionary
     with open("reverselookup.json", "r") as file:
         reverselookup = json.load(file)
+
+    # Load the trained model
     loaded_model = tf.keras.models.load_model("trainedmodel.h5")
     loaded_model.summary()
     images = []
@@ -107,11 +120,11 @@ def predict():
     cv2.imshow('Image', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+    # Binary image with some adjustments to take account of contrast intensities
     grayscale_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Adaptive thresholding
     normalized_image = cv2.adaptiveThreshold(grayscale_image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,
                                              15, 4)
-
     # Histogram equalization
     normalized_image = cv2.equalizeHist(normalized_image)
 
@@ -119,6 +132,8 @@ def predict():
     min_intensity = np.min(normalized_image)
     max_intensity = np.max(normalized_image)
     normalized_image = (normalized_image - min_intensity) / (max_intensity - min_intensity) * 255.0
+
+    # Resize and normalize
     resized_image = cv2.resize(normalized_image, (200, 200))
     img = resized_image / 255.0
     cv2.imshow('Image', img)
@@ -126,10 +141,10 @@ def predict():
     cv2.destroyAllWindows()
     images.append(img)
     images = np.array(images, dtype='float32')
+
+    # Prediction
     prediction = loaded_model.predict(images)
     predicted_label_index = np.argmax(prediction)
-    print(predicted_label_index)
-    print(reverselookup)
     predicted_label = reverselookup[str(predicted_label_index)]
     print(predicted_label)
 
